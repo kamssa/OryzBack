fa9cbab8983c8c1203263d4087c0f38cf4d4399a
kamssa <kamssa0@gmail.com>
2022-01-27 18:35:30 +0000

    change properties

---
+++ src/main/java/ci/gestion/entites/combo/Categorie.java
@@ -0,0 +1,15 @@
+package ci.gestion.entites.combo;
+
+import javax.persistence.Entity;
+
+import ci.gestion.entites.shared.AbstractEntity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categorie extends AbstractEntity{
+	private String  libelle;
+	private String description;
+}
