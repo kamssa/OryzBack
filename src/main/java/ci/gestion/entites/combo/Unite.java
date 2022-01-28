73461b03126598eaa3f1a53c22c531e8e5f5a9ec
kamssa <kamssa0@gmail.com>
2022-01-28 00:24:26 +0000

    @data

--- src/main/java/ci/gestion/entites/combo/Unite.java
+++ src/main/java/ci/gestion/entites/combo/Unite.java
@@ -4,11 +4,13 @@ import javax.persistence.Entity;
 
 import ci.gestion.entites.shared.AbstractEntity;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class Unite extends AbstractEntity{
 	private String  libelle;
 	private String description;
